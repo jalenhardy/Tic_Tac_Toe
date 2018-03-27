6aa905e27cd2f28aca052436929eae5ff6690909
Jalen Hardy <Jalenhardy93@gmail.com>
2018-03-27 19:25:12 -0400

    Removed the Player class to make game simpler

--- src/BoardGame.java
+++ src/BoardGame.java
@@ -1,27 +1,49 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class BoardGame {
+    char [][] board = new char [3][3];
 
-    private ArrayList<int[]> coordinates;
-    private HashMap<Character, int []> coordinateMap;
+    public void retrieveChords(char player){
+        Scanner scanner = new Scanner(System.in);
+        int x, y;
+        do{
+            System.out.printf("\nPlayer %c Row: ", player);
+            x = scanner.nextInt();
+        }while(x == 0);
 
-    BoardGame(){
-        coordinates = new ArrayList<>();
-        coordinateMap = new HashMap<>();
+        do{
+            System.out.printf("\nPlayer %c Column: ", player);
+            y = scanner.nextInt();
+        }while(y == 0);
+
+        board[x-1][y-1] = player;
     }
 
-    public ArrayList<int[]> getCoordinates() {
-        return coordinates;
+    public void printBoard(){
+        for (char [] arr : board){
+            for (char num : arr){
+                if (num != 0) {
+                    System.out.print(Character.toString(num) + "|");
+                }
+                else{
+                    System.out.print(" |");
+                }
+            }
+            System.out.println("\n_________");
+        }
     }
 
-    public void plot(char character, int x, int y) {
-        int [] cord = {x, y};
-        coordinates.add(cord);
-        coordinateMap.put(character, cord);
+    public static void main(String[] args) {
+        BoardGame game = new BoardGame();
+
+        game.printBoard();
+        while (true){
+            game.retrieveChords('x');
+            game.retrieveChords('o');
+            game.printBoard();
+        }
     }
 
-    public void plot(){
-        for (int [] cord : coordinateMap.get('x'));
-    }
 }
