9a3d89632eddb5f950fe12158089117b9ec9f74c
Jalen Hardy <Jalenhardy93@gmail.com>
2018-03-27 18:11:06 -0400

    Created Player and BoardGame Class

---
+++ src/BoardGame.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class BoardGame {
+    public ArrayList<int[]> getCords() {
+        return cords;
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Player {
+    private char character;
+    private BoardGame game;
+    private ArrayList<int []> coordinates;
+
+    Player(char character, BoardGame game){
+        this.character = character;
+        this.game = game;
+        coordinates = new ArrayList<>();
+    }
+
+    public void retrieveCordinates(){
+        Scanner scanner = new Scanner(System.in);
+        int x, y;
+        // If the user inputted coordinates are the same as coordinates already plotted
+        do{
+            System.out.println("X Value: ");
+            x = scanner.nextInt();
+            System.out.println("Y Value: ");
+            y = scanner.nextInt();
+
+        }while(!checkCoordinates(x, y, game.getCords()));
+    }
+
+    private boolean checkCoordinates(int x, int y, ArrayList<int []> coordinates){
+        boolean isSame = false;
+        for (int i = 0; i < coordinates.size(); i++){
+            int [] cord = coordinates.get(i);
+            if (cord[0] == x || cord[1] == y){
+                isSame = true;
+            }
+        }
+        return isSame;
+    }
+
+
+}
